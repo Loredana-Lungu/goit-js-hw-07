4036e3ecc9b9a6dee8ce42533f04592f3f06196a
Loredana-Lungu <158364067+Loredana-Lungu@users.noreply.github.com>
2024-12-02 02:12:09 +0200

    Homework-7-JS

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,59 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryContainer = document.querySelector('.gallery');
+const galleryMarkup = createGalleryMarkup(galleryItems);
+
+galleryContainer.innerHTML = galleryMarkup;
+
+function createGalleryMarkup(items) {
+  return items
+    .map(({ preview, original, description }) => {
+      return `
+        <li class="gallery__item">
+          <a class="gallery__link" href="${original}">
+            <img
+              class="gallery__image"
+              src="${preview}"
+              data-source="${original}"
+              alt="${description}"
+            />
+          </a>
+        </li>`;
+    })
+    .join('');
+}
+
+galleryContainer.addEventListener('click', onGalleryContainerClick);
+
+function onGalleryContainerClick(event) {
+  event.preventDefault();
+
+  const isGalleryImage = event.target.classList.contains('gallery__image');
+
+  if (!isGalleryImage) {
+    return;
+  }
+
+  const source = event.target.dataset.source;
+
+  openModal(source);
+}
+
+function openModal(imageSource) {
+  const instance = basicLightbox.create(`
+    <img src="${imageSource}" width="800" height="600">
+  `);
+
+  instance.show();
+
+  document.addEventListener('keydown', onEscKeyPress);
+
+  function onEscKeyPress(event) {
+    if (event.code === 'Escape') {
+      instance.close();
+      document.removeEventListener('keydown', onEscKeyPress);
+    }
+  }
+}
